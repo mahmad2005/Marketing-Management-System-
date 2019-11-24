6fe97334a4b7ac659d432826cb57d16b6ed6d9d9
mahmad2005 <m.ahmad2005@gmail.com>
2019-11-24 16:09:26 +0600

    order save added

--- http/addorderNext.php
+++ http/addorderNext.php
@@ -1,3 +1,28 @@
+﻿<?php
+if(isset($_COOKIE["U_Name"], $_COOKIE["U_Pass"]))
+ {
+	 $U_Name=$_COOKIE["U_Name"];
+	 $U_Pass=$_COOKIE["U_Pass"];
+	
+	
+	include "db_Class.php";
+	$obj = new db_class();
+	$obj->MySQL();
+
+		if($_POST == TRUE){
+			$C_Name = $_POST["C_Name"];
+			$C_Email = $_POST["C_Email"];
+			$C_Phone = $_POST["C_Phone"];
+			$C_Address = $_POST["C_Address"];
+			$TimeAndDate = date("Y-m-d H:i:s");
+			
+			
+			 $SQL = "INSERT INTO company_info (C_Name, C_Email, C_Phone, C_Address, UserName, TimeAndDate) VALUES 
+			 ('$C_Name','$C_Email','$C_Phone','$C_Address','$U_Name','$TimeAndDate')";
+			//echo $SQL;
+			$obj->sql($SQL);
+?>
+
 <html>
 <head>
 	<meta charset="utf-8" />
@@ -7,41 +32,61 @@
 </head>
 
 <body>
+		<center><h1><?php	echo $C_Name; ?></h1></center>
+		<center><p>Email: <?php	echo $C_Email ; ?></p></center>
+		<center><p>Phone: <?php	echo $C_Phone ; ?></p></center>
+		<center><p>Address: <?php	echo $C_Address; ?></p></center>
+		
 	<div class="table-title">
 		<h3>Products List</h3>
 	</div>
-	<table class="table-fill">
-		<thead>
-		<tr>
-			<th class="text-left">Item</th>
-			<th class="text-left">Quantity</th>
-		</tr>
-		</thead>
-			<tbody class="table-hover">
-				<tr>
-					<td class="text-left">Biscuit</td>
-					<td class="text-left"><input placeholder="0" type="number" name="P_Biscuit" autofocus></td>
-				</tr>
-				<tr>
-					<td class="text-left">Cake</td>
-					<td class="text-left"><input placeholder="0" type="number" name="P_Biscuit" ></td>
-				</tr>
-				<tr>
-					<td class="text-left">Chocolate</td>
-					<td class="text-left"><input placeholder="0" type="number" name="P_Biscuit" ></td>
-				</tr>
-				<tr>
-					<td class="text-left">Ice-Cream</td>
-					<td class="text-left"><input placeholder="0" type="number" name="P_Biscuit" ></td>
-				</tr>
-				<tr>
-					<td class="text-left">Bread</td>
-					<td class="text-left"><input placeholder="0" type="number" name="P_Biscuit" ></td>
-				</tr>
-		</tbody>
-	</table>
-	<fieldset>
-		<button name="submit" type="submit" id="contact-submit" data-submit="...Sending">Submit</button>
-	</fieldset>
+	<form id="contact" action="addorderSaved.php" method="post">
+		<table class="table-fill">
+			<thead>
+			<tr>
+				<th class="text-left">Item</th>
+				<th class="text-left">Quantity</th>
+			</tr>
+			</thead>
+				<tbody class="table-hover">
+					<tr>
+						<td class="text-left"><input type ="hidden" name="P_1" value="Biscuit">Biscuit</td>
+						<td class="text-left"><input placeholder="0" type="number" name="Q_1" autofocus></td>
+					</tr>
+					<tr>
+						<td class="text-left"><input type ="hidden" name="P_2" value="Cake">Cake</td>
+						<td class="text-left"><input placeholder="0" type="number" name="Q_2" ></td>
+					</tr>
+					<tr>
+						<td class="text-left"><input type ="hidden" name="P_3" value="Chocolate">Chocolate</td>
+						<td class="text-left"><input placeholder="0" type="number" name="Q_3" ></td>
+					</tr>
+					<tr>
+						<td class="text-left"><input type ="hidden" name="P_4" value="Ice-Cream">Ice-Cream</td>
+						<td class="text-left"><input placeholder="0" type="number" name="Q_4" ></td>
+					</tr>
+					<tr>
+						<td class="text-left"><input type ="hidden" name="P_5" value="Bread">Bread</td>
+						<td class="text-left"><input placeholder="0" type="number" name="Q_5" ></td>
+					</tr>
+			</tbody>
+		</table>
+		<fieldset>
+			<button name="submit" type="submit" id="contact-submit" data-submit="...Sending">Submit</button>
+		</fieldset>
+	</form>
  </body>
  </html>
+<?php			
+		
+		}
+		else
+		{
+	}
+}
+else
+{
+	echo "<p>Please Login</p>";
+}
+	
+?>
---
+++ http/addorderSaved.php
@@ -0,0 +1,50 @@
+﻿<?php
+if(isset($_COOKIE["U_Name"], $_COOKIE["U_Pass"]))
+ {
+	 $U_Name=$_COOKIE["U_Name"];
+	 $U_Pass=$_COOKIE["U_Pass"];
+	
+	
+	include "db_Class.php";
+	$obj = new db_class();
+	$obj->MySQL();
+
+		if($_POST == TRUE){
+		echo 	$P[] = $_POST["P_1"];
+		echo	$P[] = $_POST["P_2"];
+		echo	$P[]= $_POST["P_3"];
+		echo	$P[] = $_POST["P_4"];
+		echo	$P[] = $_POST["P_5"];
+		echo	$Q[] = $_POST["Q_1"];
+		echo	$Q[] = $_POST["Q_2"];
+		echo	$Q[] = $_POST["Q_3"];
+		echo	$Q[]  = $_POST["Q_4"];
+		echo	$Q[]  = $_POST["Q_5"];
+			$TimeAndDate = date("Y-m-d H:i:s");
+			
+			
+			for ($x = 0; $x <= 4; $x++) {
+			$product = $P[$x];
+			$Quantity = $Q[$x];
+				echo "The number is: $x <br>";
+				$SQL = "INSERT INTO Products (Product_Name, Product_Quantity, 	Serial_ID ) VALUES 
+				('$product ','$Quantity','3')";
+				$obj->sql($SQL);
+				}
+			//echo $SQL;
+
+?>
+
+<?php			
+		
+		}
+		else
+		{
+	}
+}
+else
+{
+	echo "<p>Please Login</p>";
+}
+	
+?>
